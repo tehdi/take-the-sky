5575853144522145913c5f85cb610882ea373763
tehdi <tehdi@users.noreply.github.com>
2019-01-12 16:01:48 -0400

    resolve #1 - track current year and current EP, and gain EP every time you advance the year

---
+++ Assets/Code/YearAdvancer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace TakeTheSky
+{
+    public class YearAdvancer : MonoBehaviour
+    {
+        private static readonly int START_YEAR = 1957; // Sputnik 1!
+        private static readonly int START_EP = 1;
+
+        private int CurrentYear = START_YEAR;
+        private int CurrentEp = START_EP;
+        private int EpGainPerYear = 1;
+
+        public Text CurrentYearText;
+        public Text CurrentEpAmountText;
+        public Button EndYearButton;
+
+        public void EndYear()
+        {
+            CurrentYear++;
+            CurrentEp += EpGainPerYear;
+
+            CurrentYearText.text = $"{CurrentYear}";
+            CurrentEpAmountText.text = $"{CurrentEp}";
+        }
+    }
+}
