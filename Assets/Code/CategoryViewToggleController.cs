8cdf46f06efb1e31c98bcda8ee8da3b227a05ab4
tehdi <tehdi@users.noreply.github.com>
2019-01-17 09:58:53 -0400

    wip #11: added toggles between target, mission, and data packet views. the views are mostly empty, but they're there now!

---
+++ Assets/Code/CategoryViewToggleController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace TakeTheSky
+{
+    public class CategoryViewToggleController : MonoBehaviour
+    {
+        public ToggleGroup CategoryViewToggleGroup;
+
+        public Toggle ViewTargetsToggleButton;
+        public GameObject TargetContainer;
+
+        public Toggle ViewMissionsToggleButton;
+        public GameObject MissionContainer;
+
+        public Toggle ViewDataPacketsToggleButton;
+        public GameObject DataPacketContainer;
+
+        void Start()
+        {
+            ToggleTargetsDisplay(true);
+        }
+
+        public void ToggleTargetsDisplay(bool isOn)
+        {
+            ToggleCategoryDisplay();
+            UtilityMethods.ChangeBackgroundColor(ViewTargetsToggleButton);
+        }
+
+        public void ToggleMissionsDisplay(bool isOn)
+        {
+            ToggleCategoryDisplay();
+            UtilityMethods.ChangeBackgroundColor(ViewMissionsToggleButton);
+        }
+
+        public void ToggleDataPacketsDisplay(bool isOn)
+        {
+            ToggleCategoryDisplay();
+            UtilityMethods.ChangeBackgroundColor(ViewDataPacketsToggleButton);
+        }
+
+        private void ToggleCategoryDisplay()
+        {
+            TargetContainer.SetActive(ViewTargetsToggleButton.isOn);
+            MissionContainer.SetActive(ViewMissionsToggleButton.isOn);
+            DataPacketContainer.SetActive(ViewDataPacketsToggleButton.isOn);
+        }
+    }
+}
